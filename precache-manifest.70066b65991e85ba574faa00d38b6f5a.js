78f124ba10fa364dfa7af5aea57e031f8418e3c9
Prathamesh-01 <prathameshshendarkar.code@gmail.com>
2020-05-27 22:52:36 +0530

    Updates

---
+++ precache-manifest.70066b65991e85ba574faa00d38b6f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d431538c843965171b4fd71208af4678",
+    "url": "/monsterproject/index.html"
+  },
+  {
+    "revision": "382fc1c5169e174ac9ba",
+    "url": "/monsterproject/static/css/main.78d8e6ac.chunk.css"
+  },
+  {
+    "revision": "82b906bcab03a02a2a46",
+    "url": "/monsterproject/static/js/2.632481a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterproject/static/js/2.632481a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382fc1c5169e174ac9ba",
+    "url": "/monsterproject/static/js/main.afd50e22.chunk.js"
+  },
+  {
+    "revision": "ce0f130334725a47c390",
+    "url": "/monsterproject/static/js/runtime-main.36f32e61.js"
+  }
+]);
